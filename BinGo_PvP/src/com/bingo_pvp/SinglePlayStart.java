50e3b56d0d37f0dd647f83106f7faf6b58a72995
cwDespair <saberwan2013@gmail.com>
2015-06-04 12:54:37 +0800

    Update SinglePlayStart.java

--- BinGo_PvP/src/com/bingo_pvp/SinglePlayStart.java
+++ BinGo_PvP/src/com/bingo_pvp/SinglePlayStart.java
@@ -17,67 +17,69 @@ import android.widget.Toast;
 
 
 public class SinglePlayStart extends Activity {
-	Button[] bt;
-	Button Startbt;
-	ArrayList<String> al= null;
-	LinearLayout Layout;
-	@Override
+    Button[] bt;
+    Button Startbt;
+    ArrayList<String> al= null;
+    LinearLayout Layout;
+    int[][] aiArray = new int[5][5];
+
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.singleplay);
         if(!Cheese.boolean1){
-        	al = new ArrayList<String>();
-        	for(int i=0;i<25;i++)
-        		al.add(((i+1)+""));
-        	Cheese.boolean1 = true;
-        	Cheese.al = al;
+            al = new ArrayList<String>();
+            for(int i=0;i<25;i++)
+                al.add(((i+1)+""));
+            Cheese.boolean1 = true;
+            Cheese.al = al;
         }else
-        	al = Cheese.al;
-        
+            al = Cheese.al;
+
         BT_Click();
     }
-    
+
     public void BT_Click(){
-    	//中間的棋盤
+        //中間的棋盤
         bt = new Button[25];
         for(int i = 0;i<25;i++){
-        	String num = (i+1)+"";       	
-        	String str = "button"+num;
-        	int id = getResources().getIdentifier(str, "id", getPackageName());
-        	bt[i] = (Button)findViewById(id);
-        	bt[i].setText(al.get(i));
-        	//長按棋盤 進入 設定 棋盤
-        	bt[i].setOnLongClickListener(new OnLongClickListener() {
-    			@Override
-    			public boolean onLongClick(View v) {
-    				// 長按棋盤 進入 設定 棋盤
-    				Intent intent = new Intent(SinglePlayStart.this, SetActivity.class);
-    				startActivity(intent);
-    				return false;
-    			}
-    		});
+            String num = (i+1)+"";
+            String str = "button"+num;
+            int id = getResources().getIdentifier(str, "id", getPackageName());
+            bt[i] = (Button)findViewById(id);
+            bt[i].setText(al.get(i));
+            //長按棋盤 進入 設定 棋盤
+            bt[i].setOnLongClickListener(new OnLongClickListener() {
+                @Override
+                public boolean onLongClick(View v) {
+                    // 長按棋盤 進入 設定 棋盤
+                    Intent intent = new Intent(SinglePlayStart.this, SetActivity.class);
+                    startActivity(intent);
+                    return false;
+                }
+            });
         }
         //開始按鈕
         Startbt = (Button)findViewById(R.id.Startbt);
-        Startbt.setOnClickListener(new OnClickListener() {		
-			@Override
-			public void onClick(View v) {
-				//單人遊戲開始 昶崴負責
-				
-			}
-		});
+        Startbt.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //單人遊戲開始 昶崴負責
+
+            }
+        });
         //長按Layout
         Layout = (LinearLayout)findViewById(R.id.SingleLayout);
         Layout.setOnLongClickListener(new OnLongClickListener() {
-			@Override
-			public boolean onLongClick(View v) {
-				// 長按棋盤 進入 設定 棋盤
-				Intent intent = new Intent(SinglePlayStart.this, SetActivity.class);
-				startActivity(intent);
-				return false;
-			}
-		});
-    	
+            @Override
+            public boolean onLongClick(View v) {
+                // 長按棋盤 進入 設定 棋盤
+                Intent intent = new Intent(SinglePlayStart.this, SetActivity.class);
+                startActivity(intent);
+                return false;
+            }
+        });
+
     }
 
     @Override
@@ -100,14 +102,35 @@ public class SinglePlayStart extends Activity {
     }
     @Override
     protected void onResume() {
-    	// TODO Auto-generated method stub
-    	super.onResume();
-    	
-    
+        // TODO Auto-generated method stub
+        super.onResume();
+
+
     }
-    
+
     //AI 設定
     public void AIcontrol(){
+
         Random rdm = new Random();
+    }
+
+    int aiCount = 0;
+    int[] aiAllow = new int[12];
+    //ai 連線計數器
+    public void aiCount(){
+        if((aiArray[0][0] == 1 && aiArray[1][1] == 1 && aiArray[2][2] == 1 && aiArray[3][3] == 1 && aiArray[4][4] == 1)){
+            aiCount++;
+        }
+        if((aiArray[0][4] == 1 && aiArray[1][3] == 1 && aiArray[2][2] == 1 && aiArray[3][1] == 1 && aiArray[4][0] == 1)){
+            aiCount++;
+        }
+        for(int i = 0; i<aiArray.length; i++) {
+            if ((aiArray[i][0] == 1 && aiArray[i][1] == 1 && aiArray[i][2] == 1 && aiArray[i][3] == 1 && aiArray[i][4] == 1)){
+                aiCount++;
+            }
+            if ((aiArray[0][i] == 1 && aiArray[1][i] == 1 && aiArray[2][i] == 1 && aiArray[3][i] == 1 && aiArray[4][i] == 1)){
+                aiCount++;
+            }
+        }
     }
 }
