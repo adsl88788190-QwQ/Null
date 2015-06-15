ab5ed7b526da4abe363e40db0d976f4a293f18ad
cwDespair <saberwan2013@gmail.com>
2015-06-15 09:29:09 +0800

    Update SinglePlayStart.java

--- BinGo_PvP/src/com/bingo_pvp/SinglePlayStart.java
+++ BinGo_PvP/src/com/bingo_pvp/SinglePlayStart.java
@@ -2,9 +2,7 @@ package com.example.soloqchess;
 
 import java.util.Random;
 
-import android.R.integer;
 import android.app.Activity;
-import android.app.Dialog;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
@@ -30,7 +28,7 @@ public class MainActivity extends Activity {
 	Button bt;
 	//ai隨機到的數字
 	int aiGet = 0;
-	
+	//是否定義一個新的ai棋盤
 	boolean gamesetup = false;
 		
     @Override
@@ -39,6 +37,9 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_main);
         tv = (TextView)findViewById(R.id.textView1);
         bt = (Button)findViewById(R.id.button1);
+        //定義新的棋盤
+      		if(!gamesetup)
+      			gameSet();
 		bt.setOnClickListener(new OnClickListener() {
 			
 			//按下button就讓ai挑一個變數
@@ -46,12 +47,19 @@ public class MainActivity extends Activity {
 			public void onClick(View v) {
 				// TODO Auto-generated method stub
 				Random ran = new Random();
+				//點擊button就隨機點名一個數字
 				aiGet = ran.nextInt(25)+1;
 				Log.d("aiGet", Integer.toString(aiGet));
+				//數字(aiGet)存回ai之boolean陣列
+				//(aiGet-1)/5=row
+				//(aiGet-1)%5=column
 				aiArrayAllow[(aiGet-1)/5][(aiGet-1)%5] = true;
+				//檢測boolean是否有變動
+				//Log.d("allow",String.valueOf((aiArrayAllow[(aiGet-1)/5][(aiGet-1)%5])?1:0));
 				//Log.d("allow", Integer.toString((aiGet-1)/5)+" "+Integer.toString((aiGet-1)%5));				
 			}
 		});
+		//刷新棋盤
 		setupText();
     }
 
@@ -74,42 +82,50 @@ public class MainActivity extends Activity {
         return super.onOptionsItemSelected(item);
     }
     
+    //定義aiStr內的棋盤
+    void gameSet(){
+    	aiStr = "";
+    	//Log.d("83", "83");
+    	for(int i=0; i<aiArray.length; i++){
+        	for(int j=0; j<aiArray[i].length; j++){
+        		aiArray[i][j] = i*5+j+1;
+        		aiArrayAllow[i][j] = false;
+        		//個位數+0
+        		if(aiArray[i][j] / 10 == 0)
+        			aiStr += 0;
+        		aiStr += aiArray[i][j]+" ";
+        		//每5個換行
+        		if(aiArray[i][j] % 5 == 0)
+        			aiStr += "\n";
+        		//Log.d("95", "95");
+        	}
+        }
+		gamesetup = true;
+    }
+    
     //刷新ai期盤
     void setupText(){
-    	if(!gamesetup){
-	    	for(int i=0; i<aiArray.length; i++){
-	        	for(int j=0; j<aiArray[i].length; j++){
-	        		aiArray[i][j] = i*5+j+1;
-	        		aiArrayAllow[i][j] = false;
-	        		//個位數+0
-	        		if(aiArray[i][j] / 10 == 0)
-	        			aiStr += 0;
-	        		aiStr += aiArray[i][j]+" ";
-	        		if(aiArray[i][j] % 5 == 0)
-	        			aiStr += "\n";
-	        	}
-	        }
-    		gamesetup = true;
-    	}
     	aiStr = "";
-    	if(gamesetup){
-    		Log.d("96", "96");
-	    	for(int i=0; i<aiArray.length; i++){
-	        	for(int j=0; j<aiArray[i].length; j++){
-	        		aiStr += " ";
-	        		if(aiArray[i][j] / 10 == 0)
-	        			aiStr += 0;
-	        		aiStr += aiArray[i][j];
-	        		if(aiArrayAllow[i][j])
-	        			aiStr += "*";
-	        		else
-	        			aiStr += " ";
-	        		if(aiArray[i][j] % 5 == 0)
-	        			aiStr += "\n";
-	        		Log.d("109", "109");
-	        	}
-	        }
-    	}
+		//Log.d("96", "96");
+    	for(int i=0; i<aiArray.length; i++){
+        	for(int j=0; j<aiArray[i].length; j++){
+        		aiStr += " ";
+        		//個位數+0
+        		if(aiArray[i][j] / 10 == 0)
+        			aiStr += 0;
+        		aiStr += aiArray[i][j];
+        		//如果該數字被點名，就標記*
+        		Log.d("r119-allow",String.valueOf((aiArrayAllow[i][j])?1:0));
+        		if(aiArrayAllow[i][j])
+        			aiStr += "*";
+        		else
+        			aiStr += " ";
+        		//每5個換行
+        		if(aiArray[i][j] % 5 == 0)
+        			aiStr += "\n";
+        		//Log.d("119", "119");
+        	}
+        }
     	Log.d("aiStr", aiStr);
     	tv.setText(aiStr);
     }
