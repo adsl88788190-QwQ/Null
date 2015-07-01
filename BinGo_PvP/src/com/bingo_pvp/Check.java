d320700b6e36ec57647b2d9b4e6d3346028c9cd6
QwQ <adsl88788190@gmail.com>
2015-07-02 01:31:21 +0800

    Add AINext & ADD Dialog

--- BinGo_PvP/src/com/bingo_pvp/Check.java
+++ BinGo_PvP/src/com/bingo_pvp/Check.java
@@ -14,49 +14,53 @@ package com.bingo_pvp;
 public class Check {
 	int SIZE = 5;
 	private boolean[][] array;
-	//建構子 
-	public Check(){
+
+	// 建構子
+	public Check() {
 		array = new boolean[5][5];
 	}
-	public Check(boolean[][] array){
-		if(array.length != 5)//若棋盤大小不為5 則更改SIZE與棋盤大小符合
+
+	public Check(boolean[][] array) {
+		if (array.length != 5)// 若棋盤大小不為5 則更改SIZE與棋盤大小符合
 			SIZE = array.length;
-		this.array = array;//棋盤放入
+		this.array = array;// 棋盤放入
 	}
-	//check.win() 回傳true 則連線達五條 獲勝;回傳false 則否
-	public boolean win(){		
-		if(line() >= 5)
+
+	// check.win() 回傳true 則連線達五條 獲勝;回傳false 則否
+	public boolean win() {
+		if (line() >= 5)
 			return true;
-		
+
 		return false;
 	}
-	//check.line() 回傳目前棋盤連線數
-	public int line(){
-		int count = 0;//連線數
-		//橫的連線
-		for(int i = 0;i<SIZE;i++){
+
+	// check.line() 回傳目前棋盤連線數
+	public int line() {
+		int count = 0;// 連線數
+		// 橫的連線
+		for (int i = 0; i < SIZE; i++) {
 			int temp = 0;
-			for(int j = 0;j<SIZE;j++)
-				if(array[i][j])
+			for (int j = 0; j < SIZE; j++)
+				if (array[i][j])
 					temp++;
-			if(temp == SIZE)
+			if (temp == SIZE)
 				count++;
 		}
-		//直的連線
-		for(int j = 0;j<SIZE;j++){
+		// 直的連線
+		for (int j = 0; j < SIZE; j++) {
 			int temp = 0;
-			for(int i = 0;i<SIZE;i++)
-				if(array[i][j])
+			for (int i = 0; i < SIZE; i++)
+				if (array[i][j])
 					temp++;
-			if(temp == SIZE)
+			if (temp == SIZE)
 				count++;
 		}
-		//斜的連線
-		if(array[0][0] &array[1][1]&array[2][2]&array[3][3]&array[4][4])
+		// 斜的連線
+		if (array[0][0] & array[1][1] & array[2][2] & array[3][3] & array[4][4])
 			count++;
-		if(array[0][4] &array[1][3]&array[2][2]&array[3][1]&array[4][0])
+		if (array[0][4] & array[1][3] & array[2][2] & array[3][1] & array[4][0])
 			count++;
-		return count; 
+		return count;
 	}
-	
+
 }
--- BinGo_PvP/src/com/bingo_pvp/SinglePlayStart.java
+++ BinGo_PvP/src/com/bingo_pvp/SinglePlayStart.java
@@ -1,17 +1,17 @@
 package com.bingo_pvp;
 
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.Collections;
 import java.util.Random;
+
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.AlertDialog.Builder;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
-import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -26,6 +26,8 @@ public class SinglePlayStart extends Activity {
 		setContentView(R.layout.singlevscomputer);
 		AIInit(); // AI初始設定
 		PlayInit(); // 玩家初始設定
+		show = (TextView)findViewById(R.id.textView2);//連線數
+		show.setText("玩家   0 : 0   電腦");
 	}
 
 	// AI初始設定
@@ -33,11 +35,15 @@ public class SinglePlayStart extends Activity {
 	// 儲存ai的陣列
 	int[][] aiArray = new int[5][5];
 	// 儲存ai是否按下
-	boolean [][] AIClick = new boolean[5][5];
-	//AI設定
+	boolean[][] AIClick = new boolean[5][5];
+
+	// AI設定
 	private void AIInit() {
 		// 設定AI初始棋盤
 		ArrayList<Integer> temp = new ArrayList<Integer>();
+		for (boolean[] temp1 : AIClick)
+			for (boolean temp2 : temp1)
+				temp2 = false;
 		// 填滿1 - 25
 		for (int i = 1; i <= 25; i++)
 			temp.add(i);
@@ -66,12 +72,16 @@ public class SinglePlayStart extends Activity {
 	// 儲存Play的陣列
 	int[][] PlayArray = new int[5][5];
 	// 儲存Play是否按下
-	boolean [][] PlayClick = new boolean[5][5];
-	//玩家設定
+	boolean[][] PlayClick = new boolean[5][5];
+
+	// 玩家設定
 	private void PlayInit() {
-		//獲得玩家棋盤
+		// 獲得玩家棋盤
 		ArrayList<String> temp = Cheese.al;
 		Play = new ImageView[5][5];
+		for (boolean[] temp1 : PlayClick)
+			for (boolean temp2 : temp1)
+				temp2 = false;
 		// 放入棋盤陣列
 		for (int i = 0; i < 25; i++)
 			PlayArray[i / 5][i % 5] = Integer.parseInt(temp.get(i));
@@ -88,13 +98,95 @@ public class SinglePlayStart extends Activity {
 			Play[i / 5][i % 5].setImageResource(id);
 		}
 	}
-	//偵測玩家按下棋盤
+
+	// 電腦等待時間
+	private boolean wait = true;
+	// 偵測玩家按下棋盤
 	OnClickListener click = new OnClickListener() {
-		
 		@Override
 		public void onClick(View v) {
 			// TODO Auto-generated method stub
-			
+			int ClickId = v.getId();
+			int id;
+			for (int i = 0; i < 25; i++) {
+				id = getResources().getIdentifier("imageView" + (i + 26), "id",
+						getPackageName());
+				// 比對看是哪個ImageView
+				if (ClickId == id && !PlayClick[i / 5][i % 5]) {
+					// setImageResource
+					id = getResources().getIdentifier(
+							"g" + PlayArray[i / 5][i % 5], "drawable",
+							getPackageName());
+					Play[i / 5][i % 5].setImageResource(id);
+					PlayClick[i / 5][i % 5] = true;
+					// 電腦顯示 同一個數字
+					for (int j = 0; j < 5; j++) {
+						for (int k = 0; k < 5; k++) {
+							if (aiArray[j][k] == PlayArray[i / 5][i % 5]) {
+								ai[j][k].setImageResource(id);
+								AIClick[j][k] = true;
+							}
+						}
+					}
+					//判斷勝負
+					if(!win()){// 電腦下一步
+						AINext();
+					}
+					break;
+				}
+			}
 		}
 	};
+	// 電腦選擇下一步
+	AIChoose aiChoose = new AIChoose();
+
+	public void AINext() {
+		int choose = aiChoose.choose(AIClick);
+		Log.d("line143", choose + "");
+		// 電腦所選的數字
+		int id = getResources().getIdentifier(
+				"g" + (aiArray[choose / 10][choose % 10]), "drawable",
+				getPackageName());
+		ai[choose / 10][choose % 10].setImageResource(id);
+		AIClick[choose / 10][choose % 10] = true;
+		// 玩家顯示
+		for (int k = 0; k < 5; k++) {
+			for (int l = 0; l < 5; l++) {
+				if (PlayArray[k][l] == aiArray[choose / 10][choose % 10]) {
+					Play[k][l].setImageResource(id);
+					PlayClick[k][l] = true;
+				}
+			}
+		}
+		//判斷勝負
+		win();
+	}
+	//判斷勝負
+	Check AICheck = new Check(AIClick);
+	Check PlayCheck = new Check(PlayClick);
+	TextView show;
+	public boolean win(){
+		//顯示行數比
+		show.setText("玩家   "+PlayCheck.line()+" : "+AICheck.line()+"  電腦");
+		//Dialog
+		AlertDialog.Builder builder = new Builder(SinglePlayStart.this);
+		builder.setTitle("賓果贏家");
+		
+		//判斷勝負
+		if(AICheck.win() & PlayCheck.win()){//平手
+			builder.setMessage("你與電腦打成了平手!!!");
+		}else if(AICheck.win()){//AI win
+			builder.setMessage("電腦碾碎你了!!!!!!!");
+		}else if(PlayCheck.win()){//Play win
+			builder.setMessage("你成功的擊敗了電腦!!!");
+		}else return false;
+		builder.setPositiveButton("確定。", new DialogInterface.OnClickListener() {
+			@Override
+			public void onClick(DialogInterface dialog, int which) {
+				dialog.dismiss();
+				//SinglePlayStart.this.finish();			
+			}
+		}).show();
+		return true;
+	}
 }
